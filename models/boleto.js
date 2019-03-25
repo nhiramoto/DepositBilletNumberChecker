0839daea2cc9e900e5dae3e5054f9c9cbc5a31e5
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-03-25 20:08:00 -0300

    Implementado método estruturaValida e testado post request.

--- models/boleto.js
+++ models/boleto.js
@@ -1,6 +1,8 @@
-let _comprimento = 47;
-let _digitoArrecadacao = '8';
-let _tipo = {
+// -*- coding: utf-8 -*-
+
+const _comprimento = 47;
+const _digitoArrecadacao = '8';
+const _tipo = {
     BANCARIO: 1,
     ARRECADACAO: 2
 };
@@ -8,21 +10,33 @@ let _tipo = {
 class Boleto {
 
     constructor(linhaDigitavel) {
-        this._linhaDigitavel = linhaDigitavel;
+        this._linhaDigitavel = linhaDigitavel.trim();
     }
 
-    static get COMPRIMENTO() {
+    /*
+     * Comprimento padrão da linha digitável (incluindo DVs).
+     */
+    static get COMPRIMENTO_PADRAO() {
         return _comprimento;
     }
 
+    /*
+     * Primeiro digito da linha digitável que define se o tipo do boleto é de arrecadação.
+     */
     static get DIGITO_ARRECADACAO() {
         return _digitoArrecadacao;
     }
 
+    /*
+     * Tipos de boletos.
+     */
     static get Tipo() {
         return _tipo;
     }
 
+    /**
+     * Tipo do boleto a partir da linha digitável.
+     */
     static tipo(linhaDigitavel) {
         if (linhaDigitavel.charAt(0) === Boleto.DIGITO_ARRECADACAO) {
             return Boleto.Tipo.ARRECADACAO;
@@ -31,6 +45,9 @@ class Boleto {
         }
     }
 
+    /**
+     * Tipo do boleto atual.
+     */
     get tipo() {
         return Boleto.tipo(this._linhaDigitavel);
     }
@@ -41,6 +58,21 @@ class Boleto {
 
     set linhaDigitavel(linhaDigitavel) {
         this._linhaDigitavel = linhaDigitavel;
+    }
+
+    /**
+     * Verifica se o comprimento é válido e se não contém símbolos não numéricos.
+     */
+    estruturaValida() {
+        if (this._linhaDigitavel.length !== Boleto.COMPRIMENTO_PADRAO) {
+            // Linha digitável maior que o comprimento padrão.
+            return false;
+        } else if (/\D/.test(this._linhaDigitavel)) {
+            // Linha digitável contém algum caracter não numérico.
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const Boleto = require('../models/boleto');
+const BoletoBancario = require('../models/boleto-bancario');
+const BoletoArrecadacao = require('../models/boleto-arrecadacao');
+
 router.post('/checa_boleto', (req, res) => {
-    console.log(req.body);
-    res.send('Boleto enviado!');
+    console.log('Requisitando POST em "/checa_boleto".');
+    if (typeof(req.body.linhaDigitavel) !== 'undefined') {
+        let linhaDigitavel = req.body.linhaDigitavel;
+        let boleto = new Boleto(linhaDigitavel);
+        res.send({
+            estruturaValida: boleto.estruturaValida(),
+            tipo: boleto.tipo,
+            linhaDigitavel: boleto.linhaDigitavel
+        });
+    } else {
+        res.send({
+            valido: false,
+            msg: 'Linha digitável não definida.'
+        });
+    }
 });
 
 module.exports = router;
